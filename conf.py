731c330e26ad80f97f7afe5ae3096fdf60684130
Leandro Pereira <leandro@hardinfo.org>
2013-07-20 21:02:38 -0300

    Add redirect from blog/ to blog/html/

--- conf.py
+++ conf.py
@@ -23,7 +23,7 @@ author = 'Leandro Pereira'
 copyright = '2013, ' + author
 
 # Change this to your blog root URL (required for RSS feed)
-website = 'http://127.0.0.1/blog/html/'
+website = 'http://tia.mat.br/blog/html/'
 
 # **************************************************************
 # More tweaks you can do
