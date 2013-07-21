64c781e69e4356cb63b3356b216c1d2c71c5b81b
Leandro Pereira <leandro@hardinfo.org>
2013-07-20 21:08:02 -0300

    Add tag cloud to the sidebar

--- conf.py
+++ conf.py
@@ -72,7 +72,7 @@ exclude_patterns = ["drafts/*", "_templates/*"]
 
 # Add templates to be rendered in sidebar here
 html_sidebars = {
-    "**": ["recent.html", "searchbox.html"]
+    "**": ["searchbox.html", "recent.html", "tags_cloud.html"]
 }
 
 # **************************************************************
