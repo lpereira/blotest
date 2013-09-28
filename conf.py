d9fd0265a091e11fd79f268a1de5c5715853cbbf
Leandro Pereira <leandro@hardinfo.org>
2013-09-27 21:30:29 -0300

    Change conf to generate 5 posts/page

--- conf.py
+++ conf.py
@@ -49,7 +49,7 @@ rss_service = None
 rss_generate_full_posts = False
 
 # Number of blog posts per page
-posts_per_page = 10
+posts_per_page = 5
 
 # **************************************************************
 # Edit lines below to further customize Sphinx build
